425e474726e17a20cf32bbcd93852779937a2514
Huy Nguyễn Dev <huynh.rtbed@gmail.com>
2025-01-24 00:51:46 +0700

    fix: fix font

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,7 @@ import "slick-carousel/slick/slick-theme.css";
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline bg-mint-500 font-primary">
-        Hello world!
-      </h1>
+      <h1 className=" font-second ">Hello world!</h1>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 // tailwind.config.js
 module.exports = {
   content: ["./src/**/*.{html,js}"], // Adjust the path to your project's file structure
-  theme: {
-    extend: {},
-  },
-  plugins: [require("tailwindcss-animated")],
+  theme: {},
+  plugins: [
+    require("tailwindcss-animated"),
+    require("@tailwindcss/typography"),
+  ],
 };
