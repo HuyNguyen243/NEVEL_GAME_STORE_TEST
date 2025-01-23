94cc5db725622c2bf4236dec240c9e608e13a6cb
Nguyen Hoang Huy <huynh@vacsvnhcm.local>
2025-01-23 18:37:08 +0700

    fix: font

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline bg-mint-500 ">
+      <h1 className="text-3xl font-bold underline bg-mint-500 font-primary">
         Hello world!
       </h1>
     </>
