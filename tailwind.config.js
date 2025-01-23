30590472d97e053896fa2373e4cb4b14bee231b9
Nguyen Hoang Huy <huynh@vacsvnhcm.local>
2025-01-23 18:26:18 +0700

    fix: animation

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+// tailwind.config.js
+module.exports = {
+  content: ["./src/**/*.{html,js}"], // Adjust the path to your project's file structure
+  theme: {
+    extend: {},
+  },
+  plugins: [require("tailwindcss-animated")],
+};
