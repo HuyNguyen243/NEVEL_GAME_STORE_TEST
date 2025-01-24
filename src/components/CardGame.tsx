f72cf5c294c92fa142c6d488eeba1bb06d7abff7
Nguyen Hoang Huy <huynh@vacsvnhcm.local>
2025-01-24 16:48:24 +0700

    fix: response

--- src/components/CardGame.tsx
+++ src/components/CardGame.tsx
@@ -15,10 +15,10 @@ const CardGame: React.FC<Iprops> = (props) => {
           src={props.image}
           alt="card game"
         />
-        <p className="absolute font-first  font-bold bottom-[100px] left-[15px] text-3xl xl:text-5xl text-white px-4">
+        <p className="absolute font-first  font-bold bottom-[100px] md:left-[15px] left-[0px] text-2xl xl:text-5xl text-white px-4">
           {props.name}
         </p>
-        <p className="absolute font-first  bottom-[40px] left-[15px] text-md text-white leading-4 line-clamp-3 px-4">
+        <p className="absolute font-first  bottom-[40px] md:left-[15px] left-[0px] text-md text-white leading-4 line-clamp-3 px-4">
           {props.content}
         </p>
       </div>
